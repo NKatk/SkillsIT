e92d5e6d9784eac4f95fbeb7478e09f0865ccbaf
Nicholas <nkatkovk15@gmail.com>
2019-02-05 17:53:28 +0200

    new image: new Navigation and footer

--- Node/routes/index.js
+++ Node/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Express' });
+});
+
+
+router.get('/courses', function(req,res,next){
+  res.render('curses', {title: 'Курсы'});
 });
 
 module.exports = router;
